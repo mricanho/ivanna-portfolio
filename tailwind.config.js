10d26b96ec8ce73a9bfeb8673f284ddc9279aa20
Miguel Ricaño <mricanho@gmail.com>
2022-05-21 17:40:22 -0500

    Add index and personalize nav

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Arial', 'sans-serif'],
+      },
+      fontSize: {
+        'xs': '13px',
+      },
+      colors: {
+        'red-ivanna': '#C91517',
+      },
+    plugins: [],
+    }
+  }
+}
